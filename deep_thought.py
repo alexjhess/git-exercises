93795c4be65150bb02964104b7a0e48a32111e88
David Tang <david.tang@epfl.ch>
2022-01-27 21:20:26 +0100

    Add question

--- deep_thought.py
+++ deep_thought.py
@@ -5,5 +5,5 @@ with open("data.json", "r") as f:
     # Load json
     data = json.load(f)
     answer = data["answer"]
-    # Print answer
-    print(answer)
+    question = data["question"]
+    print(f"{question}: \n{answer}")
